3e4a5f755b2efed8a891175157a2e9f24c5a691e
vidhesh sunilrao sawarkar <vidheshsunilraosawarkar@gmail.com>
2024-03-21 22:16:09 +0530

    backend url removed from git ignore

---
+++ frontend/config.ts
@@ -0,0 +1,2 @@
+
+export const BACKEND_URL = "https://backend.vidheshsawarkar-work.workers.dev"
--- frontend/src/components/Skeleton.tsx
+++ frontend/src/components/Skeleton.tsx
@@ -1,10 +1,10 @@
 
 
-export function Skeleton({count}){
+export function Skeleton({count}: any){
     return <div>
         <p className="text-center my-10 text-2xl text-gray-600 font-medium underline underline-offset-8 decoration-4 decoration-sky-500">Submissions</p>
         <div role="status" className="w-full divide-y divide-gray-200 animate-pulse dark:divide-gray-300 dark:border-gray-700">
-            {Array.from({ length: count }, (_, i) =>
+            {Array.from({ length: count }, (_,) =>
                 <div className="flex items-center gap-x-10 px-8">
                     <div className="h-2.5 bg-gray-100 rounded-full dark:bg-gray-300 w-28 my-6"></div>
                     <div className="h-2.5 bg-gray-100 rounded-full dark:bg-gray-300 w-28 my-6"></div>
